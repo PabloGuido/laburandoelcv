146c1d7b3f3efa17a50715a9ce76ec5f53db022f
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-06-13 17:33:44 -0300

    Player can tocuh the incorrect sections. The timer runs out. The times up cues in and out. Now starting with the correction phase.

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -8,9 +8,12 @@ public class UiManager : MonoBehaviour
 {
     public static UiManager Instance;
     [HideInInspector] public bool playerInputAllowed = true;
-    //
+    private bool gameCorrectingCv = false;
+    // Timer:
     [SerializeField] private int timer;
     TMP_Text timerText;
+    // Time's up!:
+    GameObject timesUp;
     private void Awake()
     {
         // Ver de chequear que haya uno solo. creo que no haría falta porque este script no persiste entre escenas.
@@ -22,23 +25,59 @@ public class UiManager : MonoBehaviour
         //timer = 60; // In case of need or in the final build put set timer here. Maybe we can add a setting for this, at least yes for testing.
         timerText = gameObject.transform.Find("Timer").gameObject.GetComponent<TMP_Text>();
         changeTimerText(timer);
+        // Time's Up!:
+        timesUp = gameObject.transform.Find("Times up").gameObject;
+        timesUp.SetActive(false);
+        
         // Start the timer:
         Invoke("TimerTest", 1);
     }
 
+    void Update()
+    {
+        //////// VER INPUT GET TOCUCH PARA LA PANTALLA ////////
+        //////// VER INPUT GET TOCUCH PARA LA PANTALLA ////////
+        //////// VER INPUT GET TOCUCH PARA LA PANTALLA ////////
+        //////// VER INPUT GET TOCUCH PARA LA PANTALLA ////////
+        if (gameCorrectingCv){
+            // This input starts running after the times is up and while the game is correcting the CV.
+            if (Input.GetMouseButtonDown(0)){
+                //Debug.Log("Pressed primary button.");
+            }
+        }
+    }
+
     void TimerTest(){
         timer --;
-        Debug.Log(timer);
+        //Debug.Log(timer);
         if (timer == 0){
             changeTimerText(timer);
             playerInputAllowed = false;
+            //
             Debug.Log("Times up! Execute time up func.");
+            theTimeIsUp();
+            gameCorrectingCv = true; // ESTO VA CUANDO DESAPARECE TIMES UP
+            //
             return;
         }
         else{
             Invoke("TimerTest", 1);
             changeTimerText(timer);
         }
+    }
+
+    void theTimeIsUp(){
+        if (timesUp.activeSelf){
+            timesUp.SetActive(false);
+            Debug.Log("Deactivating TimesUp! visual cue. Start the correction phase.");
+            // Start with the correction phase here.
+        }
+        else {
+            Debug.Log("Activating TimesUp! visual cue and starting timer to self Invoke again.");
+            timesUp.SetActive(true);
+            Invoke("theTimeIsUp", 3);
+        }
+
     }
 
     void changeTimerText(int time){
