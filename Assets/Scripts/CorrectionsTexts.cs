086f575c35fbf4ab574fcc19e2eb3280ae143ca7
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-06-16 12:44:47 -0300

    Moving towards sections system working nice ;D

--- Assets/Scripts/CorrectionsTexts.cs
+++ Assets/Scripts/CorrectionsTexts.cs
@@ -7,7 +7,6 @@ public class CorrectionsTexts : MonoBehaviour
     [HideInInspector] public string[] textToRender;
     [HideInInspector] public string[] correctTextToRender;
     [HideInInspector] public string[] incorrectTextToRender;
-    // [HideInInspector] public string[] markedAsIncorrectTextToRender;
     [HideInInspector] public string[] step;
     [HideInInspector] public float[] CvZoom;
 
@@ -41,40 +40,51 @@ public class CorrectionsTexts : MonoBehaviour
         CvSectionsPos[5] = Cv.transform.Find("Experience").gameObject.GetComponent<RectTransform>();
         CvSectionsPos[6] = Cv.transform.Find("Contact").gameObject.GetComponent<RectTransform>();
         // Normal text:
-        step = new string[]{"read", "animate", "read", "buildUp", "awnser", "correction", "settle", "next", "next", "next", "next", "next", "next"};
+        step = new string[]{
+        "read", "animate", "read", "buildUp", "awnser", "correction", "settle", "next", // Photo
+        "read", "buildUp", "awnser", "correction", "settle", "next",    // Name
+        
+        };
 
         textToRender = new string[100];
+        // Photo:
         textToRender[0] = "0: Muy bien, corrijamos el cv. Empecemos por la foto.";
         textToRender[1] = "1: Muy bien, corrijamos el cv. Empecemos por la foto.";
         textToRender[2] = "2: Veamos..";
         textToRender[3] = "3: La foto está...";
         textToRender[4] = "4: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
         textToRender[5] = "5: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
-        textToRender[6] = "6: ALGO MAS SOBRE LAS FOTOS.";
-        textToRender[7] = "7: YENDO HACIA LO PROX.";
-        textToRender[8] = "8: YENDO HACIA LO PROX.";
-        textToRender[9] = "9: YENDO HACIA LO PROX.";
-        textToRender[10] = "10: YENDO HACIA LO PROX.";
-        textToRender[11] = "11: YENDO HACIA LO PROX.";
-        textToRender[12] = "12: YENDO HACIA LO PROX.";
+        textToRender[6] = "6: ALGO MAS SOBRE LAS FOTOS. Vamos con lo siguiente";
+        textToRender[7] = "7: YENDO HACIA EL NOMBRE.";
+        // Name:
+        textToRender[8] = "8: REVISEMOS EL NOMBRE.";
+        textToRender[9] = "9: El nombre esta...";
+        textToRender[10] = "10:CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
+        textToRender[11] = "11: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
+        textToRender[12] = "12: ALGO MAS SOBRE EL NOMBRE. Vamos al siguiente";
+        textToRender[13] = "13: Yendo hacia habilidades.";
+
 
         // Correct text:
         correctTextToRender = new string[100];
+        //Photo:
         correctTextToRender[4] = "4: ¡La foto está muy borrosa! Muy bien por darte cuenta.";
         correctTextToRender[5] = "5: Es muy importante que en nuestra foto siempre se nos pueda ver bien.";
+        // Name:
+        correctTextToRender[10] = "10: El nombre está muy bien!.";
+        correctTextToRender[11] = "11: Por estos motivos.";
+
 
         // Not marked as Incorrect text:
         incorrectTextToRender = new string[100];
+        //Photo:
         incorrectTextToRender[4] = "4: ¡Cuidado! La foto está muy borrosa y no la marcaste!";
         incorrectTextToRender[5] = "5: Recorá, es muy importante que en nuestra foto siempre se nos pueda ver bien.";
+        // Name:
+        incorrectTextToRender[10] = "10: Cuidado! El nombre está bien y lo marcaste como incorrecto.";
+        incorrectTextToRender[11] = "11: Por estos motivos. 222";
 
 
-
-
-        // // Marked as Incorrect text:
-        // markedAsIncorrectTextToRender = new string[100];
-        // markedAsIncorrectTextToRender[4] = "4: ¡Cuidado! La foto está muy borrosa y no la marcaste!";
-        // markedAsIncorrectTextToRender[5] = "5: Recorá, es muy importante que en nuestra foto siempre se nos pueda ver bien.";
     }
 
 
