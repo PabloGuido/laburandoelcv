04c828e05a60784aea250dc1c4b8bab4161220b3
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-06-23 13:16:06 -0300

    Making the keyboard, so far so good.

---
+++ Assets/Scripts/Enter Data/Key.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class Key : MonoBehaviour
+{
+    private TMP_Text thisKey;
+    private string key;
+    //
+    private Button thisButton;
+    //
+    [SerializeField] private TMP_InputField mailField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisKey = gameObject.transform.Find("thisKey").transform.GetComponent<TMP_Text>();
+        key = thisKey.text;
+
+        thisButton = gameObject.transform.GetComponent<Button>();
+        thisButton.onClick.AddListener(addKeyToField);
+    }
+
+    void addKeyToField(){
+        Debug.Log(key);
+        mailField.text = mailField.text + key;
+        mailField.caretPosition = mailField.text.Length; 
+        mailField.Select();
+        
+        
+        Debug.Log(mailField.caretPosition);
+    }
+
+}
