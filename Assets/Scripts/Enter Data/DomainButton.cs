e68ac6d3ec7bf745af4ed8d8db2102f0dd474f32
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-06-23 18:14:16 -0300

    Drop down menu working.

---
+++ Assets/Scripts/Enter Data/DomainButton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class DomainButton : MonoBehaviour
+{
+    private Button thisButton;
+    private string domainText;
+    //
+    [SerializeField] private GameObject dropDown;
+    [SerializeField] private TMP_Text dropDownButtonText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisButton = gameObject.GetComponent<Button>();
+        domainText = gameObject.transform.Find("DomainText").GetComponent<TMP_Text>().text;
+        //
+        thisButton.onClick.AddListener(selectDomain);
+
+    }
+
+    private void selectDomain(){
+        dropDown.SetActive(false);
+        dropDownButtonText.text = domainText;
+    }
+
+}
