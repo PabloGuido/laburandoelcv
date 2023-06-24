681a21a1c10f055da37caa52913ae9e56a3acfbf
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-06-23 21:56:32 -0300

    Basic PjSelect scene. About to start working again in game scene.

---
+++ Assets/Scripts/Pj Select/Pj.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+public class Pj : MonoBehaviour
+{
+    [SerializeField] private int numberOfPj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}
