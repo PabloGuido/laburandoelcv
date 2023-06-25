d200efa918f5543a86cef986155b2ef1a062b325
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2023-06-24 21:49:56 -0300

    CV PJ3 donde, need check.

--- Assets/Scripts/CV/CorrectionScriptPJ3.cs
+++ Assets/Scripts/CV/CorrectionScriptPJ3.cs
@@ -26,39 +26,38 @@ public class CorrectionScriptPJ3 : MonoBehaviour
         //
         CvSectionsGO = new Section[7];
         Cv = mask.transform.Find("CV").gameObject;
-        CvSectionsGO[0] = Cv.transform.Find("Presentation").gameObject.GetComponent<Section>();
-        CvSectionsGO[1] = Cv.transform.Find("Education").gameObject.GetComponent<Section>();
+        CvSectionsGO[0] = Cv.transform.Find("Name").gameObject.GetComponent<Section>();
+        CvSectionsGO[1] = Cv.transform.Find("Presentation").gameObject.GetComponent<Section>();
         CvSectionsGO[2] = Cv.transform.Find("Experience").gameObject.GetComponent<Section>();
-        CvSectionsGO[3] = Cv.transform.Find("Name").gameObject.GetComponent<Section>();
+        CvSectionsGO[3] = Cv.transform.Find("Education").gameObject.GetComponent<Section>();
         CvSectionsGO[4] = Cv.transform.Find("Abilities").gameObject.GetComponent<Section>();
         CvSectionsGO[5] = Cv.transform.Find("Lenguage").gameObject.GetComponent<Section>();
         CvSectionsGO[6] = Cv.transform.Find("Contact").gameObject.GetComponent<Section>();
         // 
         CvSectionsPos = new RectTransform[7];        
-        
-        CvSectionsPos[0] = Cv.transform.Find("Presentation").gameObject.GetComponent<RectTransform>();
-        CvSectionsPos[1] = Cv.transform.Find("Education").gameObject.GetComponent<RectTransform>();
+        CvSectionsPos[0] = Cv.transform.Find("Name").gameObject.GetComponent<RectTransform>();
+        CvSectionsPos[1] = Cv.transform.Find("Presentation").gameObject.GetComponent<RectTransform>();
         CvSectionsPos[2] = Cv.transform.Find("Experience").gameObject.GetComponent<RectTransform>();
-        CvSectionsPos[3] = Cv.transform.Find("Name").gameObject.GetComponent<RectTransform>();
+        CvSectionsPos[3] = Cv.transform.Find("Education").gameObject.GetComponent<RectTransform>();
         CvSectionsPos[4] = Cv.transform.Find("Abilities").gameObject.GetComponent<RectTransform>();
         CvSectionsPos[5] = Cv.transform.Find("Lenguage").gameObject.GetComponent<RectTransform>();
         CvSectionsPos[6] = Cv.transform.Find("Contact").gameObject.GetComponent<RectTransform>();
         //
-        SectionOffset = new Transform[7];        
-        SectionOffset[0] = Cv.transform.Find("Presentation").Find("Offset").GetComponent<Transform>(); 
-        SectionOffset[1] = Cv.transform.Find("Education").Find("Offset").GetComponent<Transform>();
+        SectionOffset = new Transform[7]; 
+        SectionOffset[0] = Cv.transform.Find("Name").transform.Find("Offset").GetComponent<Transform>();  
+        SectionOffset[1] = Cv.transform.Find("Presentation").Find("Offset").GetComponent<Transform>(); 
         SectionOffset[2] = Cv.transform.Find("Experience").Find("Offset").GetComponent<Transform>();
-        SectionOffset[3] = Cv.transform.Find("Name").transform.Find("Offset").GetComponent<Transform>();
+        SectionOffset[3] = Cv.transform.Find("Education").Find("Offset").GetComponent<Transform>();
         SectionOffset[4] = Cv.transform.Find("Abilities").Find("Offset").GetComponent<Transform>();
         SectionOffset[5] = Cv.transform.Find("Lenguage").Find("Offset").GetComponent<Transform>();
         SectionOffset[6] = Cv.transform.Find("Contact").Find("Offset").GetComponent<Transform>();
         //Debug.Log(SectionOffset[4]);
         // Normal text:
         step = new string[]{
-        "read", "animate", "buildUp", "awnser", "correction", "settle", "next", // Presentation
-        "buildUp", "awnser", "correction", "settle", "next", // Education
+        "read", "animate", "buildUp", "awnser", "correction", "settle", "next", // Name
+        "buildUp", "awnser", "correction", "settle", "next", // Presentation
         "buildUp", "awnser", "correction", "settle", "next", // Experience
-        "buildUp", "awnser", "correction", "settle", "next", // AditionalEd
+        "buildUp", "awnser", "correction", "settle", "next", // Education
         "buildUp", "awnser", "correction", "settle", "next", // Abilities
         "buildUp", "awnser", "correction", "settle", "next", // Lenguage
         "buildUp", "awnser", "correction", "settle", // Contact
@@ -68,13 +67,13 @@ public class CorrectionScriptPJ3 : MonoBehaviour
 
         textToRender = new string[100];
         // Presentation:
-        textToRender[0] = "0: Muy bien, corrijamos el cv. Empecemos por el perfil.";
-        textToRender[1] = "1: Muy bien, corrijamos el cv. Empecemos por el perfil.";
+        textToRender[0] = "0: Muy bien, corrijamos el cv. Empecemos por el nombre.";
+        textToRender[1] = "1: Muy bien, corrijamos el cv. Empecemos por el nombre.";
         textToRender[2] = "2: Veamos...";    
         textToRender[3] = "3: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
         textToRender[4] = "4: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
         textToRender[5] = "5: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
-        textToRender[6] = "6: Revisemos la formación.";
+        textToRender[6] = "6: Revisemos el perfil.";
         // Education:
         textToRender[7] = "7: A ver que pasa acá...";
         textToRender[8] = "8:CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
@@ -86,19 +85,19 @@ public class CorrectionScriptPJ3 : MonoBehaviour
         textToRender[13] = "13:CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
         textToRender[14] = "14: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
         textToRender[15] = "15: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
-        textToRender[16] = "16: Y a ver esos cursos...";
+        textToRender[16] = "16: Y a ver la formación...";
         // Ad Ed:
         textToRender[17] = "17: Veamos...";
         textToRender[18] = "18: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
         textToRender[19] = "19: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
         textToRender[20] = "20: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
-        textToRender[21] = "21: Miremos las habilidades y conocimientos.";
+        textToRender[21] = "21: Miremos las habilidades.";
         // Abilities:
         textToRender[22] = "22: A ver...";
         textToRender[23] = "23: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
         textToRender[24] = "24: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
         textToRender[25] = "25: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
-        textToRender[26] = "26: Veamos los idiomas.";
+        textToRender[26] = "26: Y los idiomas...";
         // Lenguages:
         textToRender[27] = "27: Veamos...";
         textToRender[28] = "28: CORRECT OR INCORRECT TEXT HERE, NOT MENT TO BE READ.";
@@ -126,20 +125,20 @@ public class CorrectionScriptPJ3 : MonoBehaviour
 
         // Correct awnsered by player text:
         correctTextToRender = new string[100];
-        // Photo + Name:
-        correctTextToRender[3] = "3: Bien! Te diste cuenta que este perfil no nos está diciendo mucho, no?";
-        correctTextToRender[4] = "4: El perfil nos la oportunidad de poder presentar quién somos, qué buscamos y qué podemos aportar.";
-        correctTextToRender[5] = "5: Muchísimo mas concreto ahora este perfil. A enfocarse en lo que somos capaces!";
+        // Name:
+        correctTextToRender[3] = "3: Muy bien! Te diste cuenta que nos está faltando algo por acá.";
+        correctTextToRender[4] = "4: Nunca olvidemos agregar nuestra profesión o puesto al que apuntamos!";
+        correctTextToRender[5] = "5: Por cierto, en este CV no hay foto y está bien. Podes elegir tenerla como no. Continuemos.";
         // Presentation:
-        correctTextToRender[8] = "8: Bien! Los estudios están bien pero sin fechas.";
-        correctTextToRender[9] = "9: Omitir fechas o no tener precisiones puede ser un error común mientras hacemos un cv.";
-        correctTextToRender[10] = "10: Es importante que no se nos olvide esto! A ver que sigue..";
+        correctTextToRender[8] = "8: Muy bien! Este perfil queda como está.";
+        correctTextToRender[9] = "9: Está bien claro quien es y sus intenciones de búsqueda.";
+        correctTextToRender[10] = "10: Todos los perfiles pueden ser diferentes pero siempre hay que ser muy claros y concisos. Sigamos.";
+        // :Experience
+        correctTextToRender[13] = "13: Genial! Pudiste ver que había un problema con la cronología en la experiencia.";
+        correctTextToRender[14] = "14: Nuestras experiencias siempre van de la mas actual a la mas antigua.";
+        correctTextToRender[15] = "15: Algo más. Este mismo orden aplica también a la educación y formación. A ver que sigue...";
         // Education:
-        correctTextToRender[13] = "13: Seguro que dentro de esas responsabilidades haya mas detalles, no?";
-        correctTextToRender[14] = "14: Mirá la cantidad de capacidades que involucran estas actividades y que cambio hace.";
-        correctTextToRender[15] = "15: Estas experiencias pueden tener relación directa con un aviso al que deseamos postularnos! Muy clave esto!";
-        // Experience:
-        correctTextToRender[18] = "18: Bien! Dígale SÍ a los cursos adicionales y NO a las faltas ortográficas!";
+        correctTextToRender[18] = "18: Bien! Palabras mal escritas y falta de tildes no las podemos dejar pasar!";
         correctTextToRender[19] = "19: Muy importante estar atento a estas cosas.";
         correctTextToRender[20] = "20: Genial! Continuemos con el CV.";
         // Abilities:
@@ -158,20 +157,20 @@ public class CorrectionScriptPJ3 : MonoBehaviour
 
         // Wrongly awnsered by player:
         incorrectTextToRender = new string[100];
-        // Photo + Name:
-        incorrectTextToRender[3] = "3: ¡Cuidado! Quizás no te parezca pero  este perfil no nos está diciendo mucho.";
-        incorrectTextToRender[4] = "4: El perfil nos la oportunidad de poder presentar quien somos, que buscamos y que podemos aportar.";
-        incorrectTextToRender[5] = "5: Fijate ahora qué tal. Muchísimo mas concreto este perfil. A enfocarse en lo que somos capaces!";
+        // Name:
+        incorrectTextToRender[3] = "3: ¡Cuidado! Acá nos estaría faltando algo.. Qué será?";
+        incorrectTextToRender[4] = "4: Claro! No olvidemos agregar nuestra profesión o puesto al que apuntamos en el CV!";
+        incorrectTextToRender[5] = "5: Por cierto, en este CV no hay foto y está bien. Podes elegir tenerla como no. Continuemos.";
         // Presentation:
-        incorrectTextToRender[8] = "8: Ojo! Fijate bien, no te parece que falta algo?";
-        incorrectTextToRender[9] = "9: Omitir fechas o no tener precisiones puede ser un error común mientras hacemos un cv.";
-        incorrectTextToRender[10] = "10: Estemos atentos con esto para la próxima! A ver que sigue..";
+        incorrectTextToRender[8] = "8: Ojo. Encontraste algo raro de este perfil?";
+        incorrectTextToRender[9] = "9: Fijate que claro expresa quien es y sus intenciones de búsqueda.";
+        incorrectTextToRender[10] = "10: Todos los perfiles pueden ser diferentes pero siempre hay que ser muy claros y concisos. Sigamos.";
         // Education:
-        incorrectTextToRender[13] = "13: Mirá las responsabilidades, seguramente esas tareas conlleven mas detalles del que dicen..";
-        incorrectTextToRender[14] = "14: Mirá la cantidad de capacidades que involucran estas actividades y que cambio hace.";
-        incorrectTextToRender[15] = "15: Estas experiencias pueden tener relación directa con un aviso al que deseamos postularnos! Muy clave esto!";
+        incorrectTextToRender[13] = "13: Cuidado! No te diste cuenta pero hay un problema con la cronología!";
+        incorrectTextToRender[14] = "14: No olvides que nuestras experiencias siempre van de la mas actual a la mas antigua!";
+        incorrectTextToRender[15] = "15: Algo más. Este mismo orden aplica también a la educación y formación. A ver que sigue...";
         // Experience:
-        incorrectTextToRender[18] = "18: Cuidado! Dígale SÍ a los cursos adicionales y NO a las faltas ortográficas!";
+        incorrectTextToRender[18] = "18: Cuidado! Palabras mal escritas y falta de tildes no las podemos dejar pasar!";
         incorrectTextToRender[19] = "19: Muy importante estar atento a estas cosas. Mirá la corrección en el CV!";
         incorrectTextToRender[20] = "20: A no olvidar buscar por este tipo de fallas cuando armamos el CV! Sigamos.";
         // Abilities:
